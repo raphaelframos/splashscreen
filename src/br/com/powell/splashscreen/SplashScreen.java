623dcbf8b6937083e0d66463eb31daebafd2ce9c
raphaelframos <raphaelframos@gmail.com>
2012-12-15 21:01:13 -0200

    Criando projeto Splash Screen

---
+++ src/br/com/powell/splashscreen/SplashScreen.java
@@ -0,0 +1,13 @@
+package br.com.powell.splashscreen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
