99c2478de4e4bcd3b21bdab8ce8dff59eca975ce
raphaelframos <raphaelframos@gmail.com>
2012-12-15 21:13:05 -0200

    Criado metodo de carregamento da tela

---
+++ src/br/com/powell/splashscreen/SegundaTela.java
@@ -0,0 +1,7 @@
+package br.com.powell.splashscreen;
+
+import android.app.Activity;
+
+public class SegundaTela extends Activity{
+
+}
--- src/br/com/powell/splashscreen/SplashScreen.java
+++ src/br/com/powell/splashscreen/SplashScreen.java
@@ -1,13 +1,30 @@
 package br.com.powell.splashscreen;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 
 public class SplashScreen extends Activity {
 
-    @Override
+    protected static final String TAG = "SplashScreen";
+	private static final long TEMPO_ENTRADA = 2000;
+
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        new Handler().postDelayed(new Runnable() {
+			
+			public void run() {
+				try{
+					startActivity(new Intent(SplashScreen.this, SegundaTela.class));
+				}catch(ActivityNotFoundException e){
+					Log.e(TAG, e.getMessage());
+				}
+			}
+		}, TEMPO_ENTRADA);
     }
 }
