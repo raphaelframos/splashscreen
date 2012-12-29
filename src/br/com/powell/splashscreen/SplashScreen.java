879ac317dbd1e32083611b5ca8e18cfcbd6a9916
raphaelframos <raphaelframos@gmail.com>
2012-12-29 12:07:51 -0200

    Criando comentarios

--- src/br/com/powell/splashscreen/SplashScreen.java
+++ src/br/com/powell/splashscreen/SplashScreen.java
@@ -10,22 +10,27 @@ import android.util.Log;
 public class SplashScreen extends Activity {
 
     protected static final String TAG = "SplashScreen";
-	private static final long TEMPO_ENTRADA = 2000;
+	private static final long TEMPO_DURACAO_ENTRADA = 2000;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //No layout splash_screen é onde pode ser mudado a imagem de exibição
         setContentView(R.layout.splash_screen);
+        
+        //Metodo que funciona como contador para chamada da nova activity
         new Handler().postDelayed(new Runnable() {
 			
 			public void run() {
 				try{
+					//Metodo que invoca a outra activity
 					startActivity(new Intent(SplashScreen.this, SegundaTela.class));
 					Log.d(TAG, "Tela 1");
 				}catch(ActivityNotFoundException e){
 					Log.e(TAG, e.getMessage());
 				}
 			}
-		}, TEMPO_ENTRADA);
+		}, TEMPO_DURACAO_ENTRADA);
     }
 }
